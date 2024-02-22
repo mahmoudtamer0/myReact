91674ceba83fe5f5ac656ac26ebe883b8098138e
mahmoud <mamoidtamer300@gmail.com>
2024-02-22 06:24:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <SpeedInsights />
         <Routes>
-          <Route path='/' element={<h1>home</h1>} />
+          <Route path='/' element={<h1>homeee</h1>} />
           <Route path='/about' element={<h1>about</h1>} />
           <Route path='/rec' element={<h1>rec</h1>} />
         </Routes>
